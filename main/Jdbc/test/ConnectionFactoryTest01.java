92d3124be9c98cb87d58a7fd019049e72961c400
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-07-07 17:51:44 -0300

    Docker Compose | Connection

---
+++ main/Jdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,9 @@
+package main.Jdbc.test;
+
+import main.Jdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
