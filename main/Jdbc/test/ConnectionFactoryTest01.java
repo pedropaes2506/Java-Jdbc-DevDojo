aa794b4dae0657fc4150452914f9e04ad6037ff4
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-07-14 16:58:13 -0300

    ResultSetMetaData | DatabaseMetaData

--- main/Jdbc/test/ConnectionFactoryTest01.java
+++ main/Jdbc/test/ConnectionFactoryTest01.java
@@ -4,8 +4,6 @@ import lombok.extern.log4j.Log4j2;
 import main.Jdbc.dominio.Producer;
 import main.Jdbc.service.ProducerService;
 
-import java.util.List;
-
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
@@ -20,7 +18,9 @@ public class ConnectionFactoryTest01 {
 //        ProducerService.delete(9);
 //        ProducerService.update(producerToUpdate);
 //        List<Producer> producers = ProducerService.findAll();
-        List<Producer> producers = ProducerService.findByName("Mad");
-        log.info("Producers found '{}'", producers);
+//        List<Producer> producers = ProducerService.findByName("Mad");
+//        log.info("Producers found '{}'", producers);
+//        ProducerService.showProducerMetaData();
+        ProducerService.showDriverMetaData();
     }
 }
